f127493a44812abfeb4f0d5f0aff9d7d1ef5dbe4
Aliyev21 <aliyevabdu97@gmail.com>
2017-04-16 15:37:16 +0500

    asd

---
+++ TodoApp.Web/Controllers/TasksController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TodoApp.Web.Models;
+using System.Web.Mvc;
+
+namespace TodoApp.Web.Controllers
+{
+    public class TasksController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //hello
+
+        public ActionResult GetAll()
+        {
+            TasksDBEntities context = new TasksDBEntities();
+            var tasks = context.list_tasks.Select(task => new Task
+            {
+                Id = task.Id,
+                Name = task.Task,
+                CreatedDate = task.Task_date,
+            });
+            return Json(tasks, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public ActionResult Add(string task, string date)
+        {
+            TasksDBEntities context = new TasksDBEntities();
+            var newListItem = new list_tasks { Task = task, Task_date = DateTime.Parse(date) };
+            var added = context.list_tasks.Add(newListItem);
+            context.SaveChanges();
+
+            var result = new Task
+            {
+                Id = added.Id,
+                Name = added.Task,
+                CreatedDate = added.Task_date,
+            };
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            TasksDBEntities context = new TasksDBEntities();
+            var deleted = context.list_tasks.Find(id);
+            context.list_tasks.Remove(deleted);
+            context.SaveChanges();
+
+            return Json(deleted, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ TodoApp.Web/Models/Task.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TodoApp.Web.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string CreatedDateString
+        {
+            get
+            {
+                return CreatedDate.ToShortDateString();
+            }
+        }
+    }
+}
